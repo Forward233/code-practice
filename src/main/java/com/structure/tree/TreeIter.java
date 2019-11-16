9f68dce04dff365cdb1c2a6259db9124a99190c6
yanhl <yhl>
2019-11-16 18:20:34 +0800

    structure

---
+++ src/main/java/com/structure/tree/TreeIter.java
@@ -0,0 +1,26 @@
+package com.structure.tree;
+
+/**
+ * @author: yhl
+ * @DateTime: 2019/11/16 18:09
+ * @Description:
+ */
+public class TreeIter {
+
+    static class TreeNode{
+        private Object val;
+        private TreeNode leftNode;
+        private TreeNode rightNode;
+    }
+
+    /**
+     * 前序遍历：根结点 ---> 左子树 ---> 右子树
+     * 中序遍历：左子树---> 根结点 ---> 右子树
+     * 后序遍历：左子树 ---> 右子树 ---> 根结点
+     * 层次遍历：只需按层次遍历即可
+     */
+
+    /**
+     * 递归实现左、中、右遍历
+     */
+}
