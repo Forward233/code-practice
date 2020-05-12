62ade69ea3f1dd6166872fb19e9d94f1c255d2f1
Forward233 <18729041176@163.com>
2020-05-12 08:46:29 +0800

    add

--- src/main/java/com/sort/InsertSort.java
+++ src/main/java/com/sort/InsertSort.java
@@ -101,7 +101,16 @@ public class InsertSort {
         }
     }
 
-
-
+    public static void insert5(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
 
 }
