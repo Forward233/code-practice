afa95eb3c393d3b7f97c6c9887b66cf5bece4339
Forward233 <18729041176@163.com>
2021-03-30 07:12:32 +0800

    merge list

--- src/main/java/com/leetcode/linkedlist/MergeTwoLists.java
+++ src/main/java/com/leetcode/linkedlist/MergeTwoLists.java
@@ -27,7 +27,7 @@ public class MergeTwoLists {
         listNode4.next = listNode5;
         listNode5.next = listNode6;
 //        System.out.println(mergeTwoLists(listNode1, listNode4));
-        System.out.println(recursionMergeList(listNode1, listNode4));
+        System.out.println(mergeTwoLists3(listNode1, listNode4));
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -41,6 +41,7 @@ public class MergeTwoLists {
         ListNode pre = new ListNode(-1);
         ListNode end = pre;
         while (l1 != null && l2 != null) {
+            // l2小
             if (l1.val > l2.val) {
                 end.next = l2;
                 end = l2;
@@ -59,6 +60,7 @@ public class MergeTwoLists {
         return pre.next;
     }
 
+    // 124 134
     public static ListNode recursionMergeList(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
@@ -74,4 +76,43 @@ public class MergeTwoLists {
             return l2;
         }
     }
+
+    // 124 134
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            //  需要不断的向前移动结果链表。next的时候赋值才会正确
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+
+    public static ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                head.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                head.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        head.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+
 }
